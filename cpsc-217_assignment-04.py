4cc2e09a2707205e4ba63fe870565c14fd2ba06d
froggyfan <emmaberg587@gmail.com>
2025-04-02 15:49:54 -0600

    v1, & added incorrect.okti

---
+++ cpsc-217_assignment-04.py
@@ -0,0 +1,149 @@
+#
+#
+#
+#
+import sys
+from SimpleGraphics import *
+
+#
+#  Retrieve the name of the .okti file used for input.
+#
+#  Parameters:
+#   None
+#
+#  Returns:
+#   If one Command-Line Argument was provided on startup: returns CLA
+#   If no CLA was provided on startup: returns result of prompted user input
+#   If 2+ CLA were provided: reports an error, and quits program
+#
+def fileName():
+  # If user provided one command line argument, use that as the file name
+  if len(sys.argv) == 2:
+    inf = sys.argv[1]
+    print("Using",sys.argv[1],"as the input file...")
+    return inf
+
+  # If user didn't provide a command line argument, prompt them for file name
+  elif len(sys.argv) == 1:
+    inf = input("Enter the name of the input file: ")
+    print("Using",inf,"as the input file...")
+    return inf
+
+  # Else, report there were too many command line arguments, and quit program
+  else:
+    print("Too many command line arguments were given. Quitting program...")
+    close()
+    quit
+
+
+#
+#  Process the input file, and ensure that it meets the conditions required
+#  of a properly-formatted OKTI file.
+#
+#  Parameters:
+#   inf: The provided input file. 
+#
+#  Returns:
+#   If all validation checks were passed:
+#     file: The provided input file, with the first two lines trimmed off, and
+#           all newline characters removed.
+#     xdim: The x-dimension, or width, of the image in pixels
+#     ydim: The y-dimension, or height, of the image in pixels
+#
+#   If any validation checks were failed:
+#     An appropriate error message is reported, and the program is quit.
+#
+def validateFile(inf):
+  # Check that the file exists
+  try:
+    file = open(inf, "r")
+  except:
+    print("Indicated file was not found. Quitting program...")
+    close()
+    quit
+  
+  else:
+    # Check that the file is an OKTI file
+    if file.readline().rstrip() != "okti":
+      print("Indicated file was not an OKTI file. Quitting program...")
+      close()
+      quit
+
+    # Check that image dimensions are present, and are greater than zero
+    lineTwo = file.readline().rstrip()
+    try:
+      xdim = int(lineTwo.split(" ")[0])
+      ydim = int(lineTwo.split(" ")[1])
+    except:
+      print("Dimensions of indicated file were invalid. Quitting program...")
+      close()
+      quit
+    if xdim <= 0 or ydim <= 0:
+      print("Dimensions of indicated file were invalid. Quitting program...")
+      close()
+      quit
+
+    # Check that only valid pixel types are present
+    file = file.read()
+    # Remove all newline characters from input file
+    file = file.replace("\n","").replace("\r","")
+    
+    if validateTypes(file) == False:
+      print("Invalid pixel type was detected in file. Quitting program...")
+      close()
+      quit
+
+    return file, xdim, ydim
+
+
+
+# Editor's note:: could prob remove this / convert it into a general
+# pixel-type identifying thing that works per single pixel. Then in the file
+# validation function, apply this func to every pixel in the file, and see if
+# an error occurs in there. This way u don't have as many 'redundant' funcs.
+def validateTypes(inf):
+  # Make a copy of input file
+  file = inf
+  pixelCount = 0
+
+  # Check if first character in file is a valid pixel type, then trim the first
+  # X-many characters from the file, depending on which type is indicated.
+  while len(file) > 0:
+    if file[0] == "p":
+      file = file[7:]
+      pixelCount = pixelCount + 1
+      
+    elif file[0] == "d":
+      file = file[4:]
+      pixelCount = pixelCount + 1
+      
+    elif file[0] == "R" or file[0] == "I":
+      file = file[3:]
+      pixelCount = pixelCount + 1
+      
+    elif file[0] == "r" or file[0] == "i":
+      file = file[2:]
+      pixelCount = pixelCount + 1
+      
+    else:
+      return False
+
+  print(pixelCount)
+  return True
+      
+types = ["p","d","r","R","i","I"]
+
+
+
+def main():
+  # Determine name of input file
+  inf = fileName()
+  print(inf)
+  # Ensure that input file is valid
+  inf, xdim, ydim = validateFile(inf)
+  print("inf = ")
+  print(inf)
+  print("xdim =",xdim)
+  print("ydim =",ydim)
+
+main()
